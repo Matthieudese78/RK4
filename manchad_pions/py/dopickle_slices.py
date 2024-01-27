328b52aa6d83d71e892177a9931594602b960061
matthieu <66738778+Matthieudese78@users.noreply.github.com>
2024-01-27 11:28:38 +0100

    dopickle_slices : augmentation de la discretisation temporelle si besoin

--- manchad_pions/py/dopickle_slices.py
+++ manchad_pions/py/dopickle_slices.py
@@ -6,11 +6,22 @@ import pandas as pd
 import glob
 import shutil
 #%% repertoire data
-Fext = 100.
-vlimoden = 1.e-4
+  # on discretise ?
+ndiscr = 1
+linert = True
+Fext = 193.
+mu = 0.6
+xi = 0.05
+vlimoden = 1.e-5
 spinini = 0.
+dte = 1.e-6
 vlostr = int(-np.log10(vlimoden))
-namerep = f'calc_fext_{int(Fext)}_spin_{int(spinini)}_vlo_{vlostr}'
+dtstr = int(-np.log10(dte))
+xistr = int(100.*xi)
+namerep = f'calc_fext_{int(Fext)}_spin_{int(spinini)}_vlo_{vlostr}_dt_{dtstr}_xi_{xistr}_mu_{mu}'
+if (linert):
+  namerep = f'{namerep}_inert'
+
 repload = f'../{namerep}/pickle/'
 repsave = f'./pickle/{namerep}/'
 
@@ -28,9 +39,15 @@ for i,filename in enumerate(all_files):
     if (i != 0): 
       df.sort_values(by='t',inplace=True)
       df.reset_index(drop=True,inplace=True)
-      df.drop(0,inplace=True)
-
+      # df.drop(0,inplace=True)
+      if (ndiscr>1):
+        rows2keep = df.index % ndiscr == 0 
+        df = df[rows2keep]
+        df.reset_index(drop=True,inplace=True)
     dfs.append(df)
+    if i % 50:
+      combined_df = pd.concat(dfs,ignore_index=True)
+      dfs = [combined_df]
 
 combined_df = pd.concat(dfs,ignore_index=True)
 combined_df.sort_values(by='t',inplace=True)
